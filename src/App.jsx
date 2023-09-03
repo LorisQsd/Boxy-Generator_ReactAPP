5573869d16ea7e992306c3fa9541fe66b103b809
Loris QUESADO <loris.quesado@oclock.school>
2023-09-03 18:53:36 +0200

    Setup: Mise en place HTML et premiers composants

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-wrap items-center justify-center flex-grow px-10 bg-gray-100 md:flex-nowrap">
+        {/* <LeftSide /> */}
+        {/* <Visualiastion /> */}
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="p-4 border-b-2 border-gray-200">
+      <h1 className="font-semibold text-center uppercase text-sl">
+        Boxy Generator
+      </h1>
+    </header>
+  );
+}
