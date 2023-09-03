0f5e397508917dcf99523d44f69efbdff2c17358
Loris QUESADO <loris.quesado@oclock.school>
2023-09-03 19:37:00 +0200

    Layouts: Création layout BoxPanel et BoxShadow avec leur composant

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import LeftContainer from "./layouts/LeftContainer";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-wrap items-center justify-center flex-grow px-10 bg-gray-100 md:flex-nowrap">
-        {/* <LeftSide /> */}
+        <LeftContainer />
         {/* <Visualiastion /> */}
       </main>
       <Footer />
