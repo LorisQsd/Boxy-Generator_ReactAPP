bbf9a104565f8696bf9ff0c763cb2527a3d3f2b5
Loris QUESADO <loris.quesado@oclock.school>
2023-09-03 21:13:46 +0200

    Feat: BoxColorPicker - Dynamisation (controlled inputs)

--- src/layouts/BoxPanel/BoxColorPicker.jsx
+++ src/layouts/BoxPanel/BoxColorPicker.jsx
@@ -1,5 +1,35 @@
-export default function BoxColorPicker() {
+import { useDispatch } from "react-redux";
+import { updateBoxValue } from "../../features/boxProperties";
+
+export default function BoxColorPicker({ inputData }) {
+  const dispatch = useDispatch();
+
+  function handleInputs(e) {
+    dispatch(
+      updateBoxValue({
+        inputNumber: inputData.inputNumber,
+        value: e.target.value,
+      })
+    );
+  }
+
   return (
-    <div>BoxColorPicker</div>
-  )
+    <div className="mt-3">
+      <p>{inputData.name}</p>
+      <div className="flex mt-2">
+        <input
+          value={inputData.value}
+          onChange={handleInputs}
+          className="flex-grow px-2 py-1 border focus:outline-1 outline-gray-400"
+          type="text"
+        />
+        <input
+          value={inputData.value}
+          onChange={handleInputs}
+          className="cursor-pointer h-[40px]"
+          type="color"
+        />
+      </div>
+    </div>
+  );
 }
