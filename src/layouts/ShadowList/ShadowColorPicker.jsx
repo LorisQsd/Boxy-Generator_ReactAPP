3b2eaf9087022d4c4767192b89d8e1ca240b9be7
Loris QUESADO <loris.quesado@oclock.school>
2023-09-04 20:58:22 +0200

    Feat: ShadowColorPicker - Gestion de l'input color des ombres

--- src/layouts/ShadowList/ShadowColorPicker.jsx
+++ src/layouts/ShadowList/ShadowColorPicker.jsx
@@ -1,5 +1,35 @@
-export default function ShadowColorPicker() {
+import { useDispatch } from "react-redux";
+import { updateShadowValue } from "../../features/shadows";
+
+export default function ShadowColorPicker({ inputData, shadowID }) {
+  const dispatch = useDispatch();
+
+  function handleInputs(e) {
+    dispatch(
+      updateShadowValue({
+        inputNumber: inputData.inputNumber,
+        value: e.target.value,
+        shadowID
+      })
+    );
+  }
   return (
-    <div>ShadowColorPicker</div>
-  )
+    <div className="mt-3">
+      <p>{inputData.name}</p>
+      <div className="flex mt-2">
+        <input
+          value={inputData.value}
+          onChange={handleInputs}
+          className="flex-grow px-2 py-1 border focus:outline-1 outline-gray-400"
+          type="text"
+        />
+        <input
+          value={inputData.value}
+          onChange={handleInputs}
+          className="cursor-pointer h-[40px]"
+          type="color"
+        />
+      </div>
+    </div>
+  );
 }
