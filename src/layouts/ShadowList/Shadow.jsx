6104487c437b24b5930251152457ea31de75e4fb
Loris QUESADO <loris.quesado@oclock.school>
2023-09-13 19:20:59 +0200

    Feature: Button remove a shadow

--- src/layouts/ShadowList/Shadow.jsx
+++ src/layouts/ShadowList/Shadow.jsx
@@ -3,29 +3,47 @@ import chevron from "../../assets/chevron.svg";
 import ShadowColorPicker from "./ShadowColorPicker";
 import ShadowRange from "./ShadowRange";
 import ShadowCheckbox from "./ShadowCheckbox";
+import { removeShadow } from "../../features/shadows";
+import { useDispatch } from "react-redux";
 
 export default function Shadow({ panelNumber, shadow }) {
+  const dispatch = useDispatch();
   const [toggleShadow, setToggleShadow] = useState(false);
 
   // Effet présent uniquement pour permettre à la première shadow de s'ouvrir au premier rendu
   // A la création d'autre shadow, le container sera fermé
   useEffect(() => {
     if (panelNumber === 1) {
-        setToggleShadow(true)
+      setToggleShadow(true);
     }
-  }, [])
+  }, []);
 
   const shadowInputs = shadow.inputs.map((input, index) => {
     if (input.type === "range") {
-      return <ShadowRange key={index} inputData={shadow.inputs[index]} shadowID={shadow.id} />;
+      return (
+        <ShadowRange
+          key={index}
+          inputData={shadow.inputs[index]}
+          shadowID={shadow.id}
+        />
+      );
     } else if (input.type === "color") {
-      return <ShadowColorPicker key={index} inputData={shadow.inputs[index]} shadowID={shadow.id} />;
+      return (
+        <ShadowColorPicker
+          key={index}
+          inputData={shadow.inputs[index]}
+          shadowID={shadow.id}
+        />
+      );
     }
   });
 
   return (
     <li className="border-b border-gray-300 bg-gray-50">
-      <button className="flex items-center justify-between w-full px-6 py-4 hover:bg-gray-100" onClick={() => setToggleShadow(!toggleShadow)}>
+      <button
+        className="flex items-center justify-between w-full px-6 py-4 hover:bg-gray-100"
+        onClick={() => setToggleShadow(!toggleShadow)}
+      >
         <span>Shadow {panelNumber}</span>
         <img
           style={{
@@ -41,7 +59,10 @@ export default function Shadow({ panelNumber, shadow }) {
           <div className="flex items-end px-6 pt-4">
             <ShadowCheckbox name={"active"} shadowID={shadow.id} />
             <ShadowCheckbox name={"inset"} shadowID={shadow.id} />
-            <button className="px-3 py-1 ml-auto text-sm text-white bg-red-600 rounded hover:bg-red-700">
+            <button
+              onClick={() => dispatch(removeShadow(shadow.id))}
+              className="px-3 py-1 ml-auto text-sm text-white bg-red-600 rounded hover:bg-red-700"
+            >
               Remove
             </button>
           </div>
