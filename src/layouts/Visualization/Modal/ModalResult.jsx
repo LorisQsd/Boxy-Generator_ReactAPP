e92c51d67668dfd7785a72025df0650ff7374ed1
Loris QUESADO <loris.quesado@oclock.school>
2023-09-13 19:09:19 +0200

    Modal: Fonction copier la propriété box-shadow

--- src/layouts/Visualization/Modal/ModalResult.jsx
+++ src/layouts/Visualization/Modal/ModalResult.jsx
@@ -1,8 +1,33 @@
 import getBoxShadowValue from "../../../utils/getBoxShadowValue";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 export default function ModalResult({ closeModal }) {
   const shadowValues = useSelector((state) => state.shadows);
+
+  useEffect(() => {
+    document.body.style.overflowY = "hidden";
+
+    return () => (document.body.style.overflowY = "auto");
+  }, []);
+
+  let runningAnimation = false;
+  function handleCopy(e) {
+    if (!runningAnimation) {
+      runningAnimation = true;
+      e.target.textContent = "Copied !";
+
+      setTimeout(() => {
+        e.target.textContent = "Copy";
+        runningAnimation = false;
+      }, 1250);
+    }
+
+    navigator.clipboard.writeText(
+      `box-shadow: ${getBoxShadowValue(shadowValues)}`
+    );
+  }
+
   return (
     <div
       onClick={closeModal}
@@ -14,7 +39,10 @@ export default function ModalResult({ closeModal }) {
       >
         <div className="flex items-end mb-5">
           <p className="mr-5 font-semibold">Here is your code 🎉</p>
-          <button className="px-3 py-1 ml-auto mr-2 text-sm text-white bg-blue-600 rounded hover:bg-blue-700">
+          <button
+            onClick={handleCopy}
+            className="px-3 py-1 ml-auto mr-2 text-sm text-white bg-blue-600 rounded hover:bg-blue-700"
+          >
             Copy
           </button>
           <button
